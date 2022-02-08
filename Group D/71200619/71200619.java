db1c4d70e027b5eef26de2facacbf25346e0b6ff
untungmangkunegara <untung.mangkunegara@ti.ukdw.ac.id>
2022-02-08 21:51:27 +0700

    File kode hitung perulangan java.

---
+++ Group D/71200619/71200619.java
@@ -0,0 +1,12 @@
+//71200619-Untung Mangkunegara
+
+class sum { 
+    public static void main (String args[]){ 
+        int n = 619;  //3 angka NIM dari belakang NIM 
+        int sum = 0; 
+        for(int i=1;i<=n;i++) { 
+            sum = sum + i ;
+            }
+        System.out.println("Hasil Penjumlahan " + sum); 
+        } 
+}
